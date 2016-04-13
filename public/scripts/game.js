cceedd467eac24e1dfaf2e61cdb99d3b0629126c
hugopiso@gmail.com <hugopiso@gmail.com>
2016-04-13 14:24:10 +0200

    matching btn with item

--- public/scripts/game.js
+++ public/scripts/game.js
@@ -1,25 +1,20 @@
 var btns = [
-    {id: 1, btn: "btn btn-primary", value: "Primary"},
-    {id: 2, btn: "btn btn-success", value: "Success"}
+    {id: 1, btn: "btn btn-primary", color:{backgroundColor: '#fff'}, value: "white"},
+    {id: 2, btn: "btn btn-success", color:{backgroundColor: '#000'}, value: "Black"}
 ];
 
 var items = [
-    {id: 2, btn: "btn btn-primary", value: "Primary"},
-    {id: 3, btn: "btn btn-success", value: "Success"},
-    {id: 4, btn: "btn btn-warning", value: "Warning"},
+    {id: 3, btn: "btn btn-success", color:{backgroundColor: '#fff'}, value: "White"},
+    {id: 4, btn: "btn btn-warning", color:{backgroundColor: '#000'}, value: "Black"},
 ];
-
-var colors = {
-    backgroundColor: '#7E4D76',
-};
 
 
 var GameBox = React.createClass({
     render: function() {
         return (
             <div>
-                <ItemList items={this.props.items} />
-                <BtnActionList btns={this.props.btns} />
+                <ItemList     items={this.props.items} />
+                <BtnActionList btns={this.props.btns} current={this.props.items[0]} />
             </div>
         )
     }
@@ -27,9 +22,9 @@ var GameBox = React.createClass({
 
 var ItemList = React.createClass({
     render: function() {
-        var itemsDisplay = this.props.items.map(function(item) {
+        var itemsDisplay = this.props.items.map(function(item, i) {
             return (
-                <a className={item.btn}  key={item.id}>
+                <a className={item.btn} style={item.color} key={i}>
                     {item.value}
                 </a>
             );
@@ -45,18 +40,20 @@ var ItemList = React.createClass({
 
 var BtnActionList = React.createClass({
     getInitialState: function () {
-        return { score: 0 };
+        return {
+            score: 0,
+        };
     },
-    handleClick: function () {
-        if (this.state.score < 10) {
+    handleClick: function (btnObject) {
+        if (this.props.current.color.backgroundColor == btnObject.color.backgroundColor) {
             this.setState({score: this.state.score + 1 });
         }
     },
 
     render: function() {
-        var btnActions = this.props.btns.map(function(btnAction) {
+        var btnActions = this.props.btns.map(function(btnAction, i) {
             return (
-                <a className={btnAction.btn} style={colors} onClick={this.handleClick} key={btnAction.id}>
+                <a className={btnAction.btn} style={btnAction.color} onClick={this.handleClick.bind(null, btnAction)} key={i}>
                     {btnAction.value}
                 </a>
             );
